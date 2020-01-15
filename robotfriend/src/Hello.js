c6d2434da454c7a6e21cc884e913259ca69e1d1a
Cristiane Abreu <bjwmcd@yahoo.com>
2020-01-14 21:04:15 -0500

    update

---
+++ robotfriend/src/Hello.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Hello extends React.Component{
+  render(){
+    return (
+    <div className="Hello">
+      <header className="hello2">
+        
+        <h1>Hello</h1>
+      </header>
+    </div>
+  );
+  }
+}
+
+export default Hello;
