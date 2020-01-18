93376c797920336bb994cb61a84c3d6c75fedaa0
Cristiane Abreu <bjwmcd@yahoo.com>
2020-01-18 06:02:04 -0500

    update

--- robotfriend/src/Card.js
+++ robotfriend/src/Card.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import reactDom from 'react-dom';
-import Yorkie from './Yorkie.jpg';
+import './Card.css';
+
 
 
 const Card = () =>{
     return(
-        <div className='mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10'>
-            <img alt =" robotPhoto"src={'${Yorkie}'} className='br-100 h4 w4 dib ba b--black-05 pa2'/>
+        <div id="cardComponent">
+        <div className=' mw5 center bg-light-pink br3 pa3 pa4-ns mv3 ba b--black-10 grow shadow-5'>
+            <img alt =" robotPhoto"src='https://robohash.org/cristiane' className='br-100 h4 w4 dib ba b--black-05 pa2'/>
             <div>
                 <h2 className="f3 mb2">Jane Doe</h2>
                 <p className="f5 fw4 gray mt0">jane.doe@gmail.com</p>
             </div>
+        </div>
         </div>
     );
 }
